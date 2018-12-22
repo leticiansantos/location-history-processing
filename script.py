aa0c042c39c1474eb52d771d258b71fb9c05655a
Leticia dos Santos <sletic@amazon.com>
2018-12-21 22:01:25 -0200

    Read json file

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 from geopy.geocoders import Nominatim
 import time
+import json
 
 MAX = 5
 
@@ -13,25 +14,30 @@ def getLocation(lat, lon):
     geolocator = Nominatim()
     count = 0
     while(count < MAX):
-        print(count)
+        # print(count)
         try:
             location = geolocator.reverse(formatIe7Coord(lat, lon), addressdetails=True)
+            # print(location.raw)
             return {
                 "city" : location.raw['address']['city'],
                 "county" : location.raw['address']['county'],
                 "state" : location.raw['address']['state'],
                 "country" : location.raw['address']['country'],
-                "suburb" : location.raw['address']['suburb'],
-                "neighbourhood" : location.raw['address']['neighbourhood']
+                "suburb" : location.raw['address']['suburb']
+                # "neighbourhood" : location.raw['address']['neighbourhood']
             }
             break
         except Exception as e:
-            print(e)
+            # print(e)
             time.sleep(10)
             count += 1
             pass
 
-lat = 476179045
-lon = -1223445753
-loc = getLocation(lat, lon)
-print(loc)
+def parseJsonFile(filename):
+    with open(filename, 'r') as f:
+        return json.load(f)
+
+obj = parseJsonFile("sample.json")
+for loc in obj['locations']:
+    place = getLocation(loc['latitudeE7'], loc['longitudeE7'])
+    print(place)
